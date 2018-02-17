1055f8bc2bea6607d5cb5b42aa66ce2c15104e29
Tayo Amuneke <jastworld@Tayos-MacBook-Pro.local>
2018-02-17 12:31:19 -0500

    FInished

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,5 @@
 from flask import Flask
-from config import Config
-
 app = Flask(__name__)
 app.debug = True
 
-app.config.from_object(Config)
-app.config["CACHE_TYPE"] = "null"
 from app import routes
